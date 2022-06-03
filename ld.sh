b72ec3ed8a0ec19f8262e29dd95fecd9f65734e9
DimaBerny <101942300+DimaBerny@users.noreply.github.com>
2022-06-03 11:10:49 +0300

    Add files via upload

---
+++ ld.sh
@@ -0,0 +1,1 @@
+/usr/bin/ld --build-id --no-add-needed --eh-frame-hdr --hash-style=gnu -m elf_x86_64 -dynamic-linker /lib64/ld-linux-x86-64.so.2 -o read_data /usr/lib/gcc/x86_64-redhat-linux/4.8.5/../../../../lib64/crt1.o /usr/lib/gcc/x86_64-redhat-linux/4.8.5/../../../../lib64/crti.o $
